1954fa7ba69f6fce223b78108181e74a8ce74749
alanheredero11@gmail.com <alanheredero11@gmail.com>
2022-11-15 16:14:43 +0100

    index up

--- Retos2/reto4/index.js
+++ Retos2/reto4/index.js
@@ -2,9 +2,6 @@ let R = require('./readConsole')
 let W = require('./writeAndReadObject')
 
 
-async function prueba(){
-    await R.readConsole(console.log)
-    await W.writeAndRead("Persona.json", {nombre: "alan", apellido: "heredero", edad: 22});
-}
-
-prueba()
+R.readConsole(function (obj) {
+    W.writeAndRead("Persona.json", obj)
+})
